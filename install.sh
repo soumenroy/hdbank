815c8b5339cd95dfab78d8b95e8dcae65d22f4a5
Soumen Roy <roy.soumen61@gmail.com>
2022-07-26 03:51:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,10 @@ git checkout lalsuite-v7.3
 
 git apply modify.patch
 conda env create --name hdb -f conda/environment.yml
+
+# https://stackoverflow.com/questions/34534513/calling-conda-source-activate-from-bash-script
 eval "$(conda shell.bash hook)"
+
 conda activate hdb
 ./00boot
 ./configure --prefix=$CONDA_PREFIX --enable-swig-python --disable-all-lal  --enable-lalsimulation --enable-lalinspiral --enable-lalframe --enable-lalmetaio --enable-lalburst
